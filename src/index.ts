dbf4e8869b907ac6fc713d16a9003b8804c12020
luansantosdev.py <luansantosdev.py@gmail.com>
2023-06-06 18:21:23 -0300

    feat: add index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ AppDataSourse.initialize().then(async () => {
 
 	app.use(express.json())
 
-    app.use('/api/v1', userRoutes)
-
 	return app.listen(process.env.PORT, function () {
         console.log(`listening on port ${process.env.PORT}!`);
       })
